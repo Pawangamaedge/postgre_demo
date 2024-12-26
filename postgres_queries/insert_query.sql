1c5cc8c37e11990ffd3233c14c90033cd28862b9
pawangamaedge <pawan.dabi@gammaedge.io>
2024-12-26 16:35:03 +0530

    insertion query and data csv file has been added

---
+++ postgres_queries/insert_query.sql
@@ -0,0 +1,5 @@
+insert into orgs (id, total_emp, department, location)
+values (2,5,'tester','bhopal'),
+(3,6,'QA testing','mumbai'),
+(4,3,'web developer','banglore'),
+(5,4,'python developer','pune');
